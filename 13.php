7aad21819bedd5a28548f1e806c3ecb948451474
kivik92 <vpkpoltavadkp@gmail.com>
2019-03-23 18:27:19 +0200

    12-14.php

---
+++ 13.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 23.03.19
+ * Time: 18:04
+ */
+echo "Вывести таблицу умножений \n";
+
+for($i = 1; $i <= 10; $i++) {
+    for($k = 1; $k <= 10; $k++){
+        echo "$i * $k ="  .$i*$k."\n";
+    }
+}
